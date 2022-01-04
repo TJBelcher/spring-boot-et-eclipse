2c53da05ae6c336440b4ae3524fd5368df830fd4
Thomas Belcher <tbelcher@verizon.net>
2022-01-04 14:52:56 -0500

    Adding image to app

---
+++ bootingweb/src/main/java/com/tombelcher/lil/sbet/bootingweb/BootingwebApplication.java
@@ -0,0 +1,13 @@
+package com.tombelcher.lil.sbet.bootingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootingwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootingwebApplication.class, args);
+	}
+
+}
